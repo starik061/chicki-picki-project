93d88578d9045c3f319bf6f952de425bfdb1f0ac
starik061 <starik061@gmail.com>
2023-02-15 12:32:02 +0200

    Second part changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
   root: 'src',
-  build: {
+    build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        hotel-page: resolve(__dirname, 'src/hotel-page.html'),
+        restaurant- page: resolve(__dirname, 'src/restaurant-page.html'),
+        room-page: resolve(__dirname, 'src/room-page.html'),
+      },
+    },
     outDir: '../dist',
   },
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
