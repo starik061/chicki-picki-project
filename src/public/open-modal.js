4c903564ccf4ed29e3b29a190c634a53c644698e
N0T4 <118122479+N0T4@users.noreply.github.com>
2023-02-19 13:53:25 +0000

    modal

--- src/public/open-modal.js
+++ src/public/open-modal.js
@@ -1,17 +1,28 @@
 const openModalBtn = document.getElementById("open-modal-btn");
 const modal = document.getElementById("modal");
-const closeBtn = document.querySelector(".close");
+const modalContent = modal.querySelector(".modal-content");
+const closeBtn = modal.querySelector(".close");
 
 openModalBtn.addEventListener("click", function() {
   modal.style.display = "block";
+  modalContent.classList.remove("hide");
+  modalContent.classList.add("show");
 });
 
 closeBtn.addEventListener("click", function() {
-  modal.style.display = "none";
+  modalContent.classList.remove("show");
+  modalContent.classList.add("hide");
+  setTimeout(function() {
+    modal.style.display = "none";
+  }, 400);
 });
 
 window.addEventListener("click", function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modalContent.classList.remove("show");
+    modalContent.classList.add("hide");
+    setTimeout(function() {
+      modal.style.display = "none";
+    }, 400);
   }
 });
