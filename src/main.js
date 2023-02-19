e49b540cc4fa7fba938fa7dbf7a0b674e55af453
starik061 <starik061@gmail.com>
2023-02-19 22:49:07 +0200

    check with import

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import './js/slider.js';
+import './js/swiper-restaurant.js';
+import './js/open-modal.js';
+import './js/modal-table.js';
+
 ///////////////Слайдер для секцій "детально про готель і ресторан"////////////////
 $('.details-slider').slick({
     infinite: true,
@@ -6,3 +11,4 @@ $('.details-slider').slick({
     prevArrow: "<img src='./img/details-of-our-restaurant/cil_arrow-left.svg' class='prev' alt='left'>",
     nextArrow: "<img src='./img/details-of-our-restaurant/cil_arrow-right.svg' class='next' alt='right'>"
   });
+
