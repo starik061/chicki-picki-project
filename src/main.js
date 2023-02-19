86e3175d1839d0cd1e3fcc1b295aa7b9e4666ba1
starik061 <starik061@gmail.com>
2023-02-19 21:50:40 +0200

    combine all js in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import './js/slider.js';
+import './js/swiper-restaurant.js';
+import './js/open-modal.js';
+import './js/modal-table.js';
 
 ///////////////Слайдер для секцій "детально про готель і ресторан"////////////////
 $('.details-slider').slick({
