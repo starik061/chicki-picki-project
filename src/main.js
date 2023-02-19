6c7ab9621652ae078c0e212053fcffd2cd196460
starik061 <55664753+starik061@users.noreply.github.com>
2023-02-19 23:17:28 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import './js/open-modal.js';
 import './js/modal-table.js';
 
 ///////////////Слайдер для секцій "детально про готель і ресторан"////////////////
-$('.details-slider').slick({
+/*$('.details-slider').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     prevArrow: "<img src='./img/details-of-our-restaurant/cil_arrow-left.svg' class='prev' alt='left'>",
     nextArrow: "<img src='./img/details-of-our-restaurant/cil_arrow-right.svg' class='next' alt='right'>"
-  });
+  });*/
 
