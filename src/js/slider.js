f07f10b7f7fc29fceb11cf159782370b3cb1bd27
starik061 <55664753+starik061@users.noreply.github.com>
2023-02-20 00:49:54 +0200

    Merge slick sliders code

--- src/js/slider.js
+++ src/js/slider.js
@@ -9,3 +9,10 @@ $(document).ready(function () {
     // autoplaySpeed: 1500,
   });
 });
+$('.details-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: "<img src='./img/details-of-our-restaurant/cil_arrow-left.svg' class='prev' alt='left'>",
+    nextArrow: "<img src='./img/details-of-our-restaurant/cil_arrow-right.svg' class='next' alt='right'>"
+  });
