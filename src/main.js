d605c82fb21ee9f8eb2d2ac47b4187c59b3abefc
KalchenkoNatalya <janatka2222@gmail.com>
2023-02-19 21:26:04 +0200

    Стрілки Slick

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ $('.details-slider').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    prevArrow: "<img src='img/details-of-our-restaurant/cil_arrow-left.svg' class='prev' alt='left'>",
+    prevArrow: '.btn-to-left',
     nextArrow: "<img src='img/details-of-our-restaurant/cil_arrow-right.svg' class='next' alt='right'>"
   });
+
+ // prevArrow: '<button type="button" class="btn-to-left"></button>',
+
+  // $('.details-slider').slick({
+  //   infinite: true,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   prevArrow: "<img src='img/details-of-our-restaurant/cil_arrow-left.svg' class='prev' alt='left'>",
+  //   nextArrow: "<img src='img/details-of-our-restaurant/cil_arrow-right.svg' class='next' alt='right'>"
+  // });
+
+ 
