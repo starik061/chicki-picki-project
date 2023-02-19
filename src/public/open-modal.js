8666f09542e73aeaf234b0367e259b9d75cc375f
N0T4 <118122479+N0T4@users.noreply.github.com>
2023-02-19 16:18:52 +0000

    modal

--- src/public/open-modal.js
+++ src/public/open-modal.js
@@ -1,87 +1,38 @@
-// $(document).ready(function() {
-//   const $openModalBtns = $(".open-modal-btn");
-//   const $modals = $(".modal");
-//   const $closeBtns = $(".close");
-
-//   $openModalBtns.on("click", function() {
-//     const modalTarget = $(this).data("modal-target");
-//     const $modal = $("#" + modalTarget);
-//     const $modalContent = $(".modal-content", $modal);
-
-//     $modal.css("display", "block");
-//     $modalContent.removeClass("hide").addClass("show");
-//   });
-
-//   $closeBtns.on("click", function() {
-//     const modalTarget = $(this).data("modal-target");
-//     const $modal = $("#" + modalTarget);
-//     const $modalContent = $(".modal-content", $modal);
-
-//     $modalContent.removeClass("show").addClass("hide");
-//     setTimeout(function() {
-//       $modal.css("display", "none");
-//     }, 400);
-//   });
-
-//   $modals.on("click", function(event) {
-//     if (event.target === this) {
-//       const $modalContent = $(".modal-content", $(this));
-
-//       $modalContent.removeClass("show").addClass("hide");
-//       setTimeout(function() {
-//         $(this).css("display", "none");
-//       }.bind(this), 400);
-//     }
-//   });
-// });
-
-
-$(document).ready(function() {
+ $(document).ready(function() {
   const $openModalBtns = $(".open-modal-btn");
-  const $modals = $(".modal");
-  const $closeBtns = $(".close");
+   const $modals = $(".modal");
+   const $closeBtns = $(".close");
 
-  $openModalBtns.on("click", function() {
+   $openModalBtns.on("click", function() {
+     const modalTarget = $(this).data("modal-target");
+     const $modal = $("#" + modalTarget);
+     const $modalContent = $(".modal-content", $modal);
+
+     $modal.css("display", "block");
+     $modalContent.removeClass("hide").addClass("show");
+   });
+
+   $closeBtns.on("click", function() {
     const modalTarget = $(this).data("modal-target");
-    const $modal = $("#" + modalTarget);
-    const $modalContent = $(".modal-content", $modal);
+     const $modal = $("#" + modalTarget);
+     const $modalContent = $(".modal-content", $modal);
 
-    $modal.css("display", "block");
-    $modalContent.removeClass("hide").addClass("show");
-  });
+     $modalContent.removeClass("show").addClass("hide");
+     setTimeout(function() {
+       $modal.css("display", "none");
+     }, 400);
+   });
 
-  $closeBtns.on("click", function() {
-    const modalTarget = $(this).data("modal-target");
-    const $modal = $("#" + modalTarget);
-    const $modalContent = $(".modal-content", $modal);
-
-    $modalContent.removeClass("show").addClass("hide");
-    setTimeout(function() {
-      $modal.css("display", "none");
-    }, 400);
-  });
-
-  $modals.on("click", function(event) {
-    if (event.target === this) {
-      const $modalContent = $(".modal-content", $(this));
+   $modals.on("click", function(event) {
+     if (event.target === this) {
+       const $modalContent = $(".modal-content", $(this));
 
       $modalContent.removeClass("show").addClass("hide");
-      setTimeout(function() {
-        $(this).css("display", "none");
-      }.bind(this), 400);
-    }
-  });
+       setTimeout(function() {
+         $(this).css("display", "none");
+       }.bind(this), 400);
+     }
+   });
+ });
 
-  $(window).on("beforeunload", function() {
-    $modals.each(function() {
-      const $modal = $(this);
-      const $modalContent = $(".modal-content", $modal);
-
-      $modalContent.removeClass("show").addClass("hide");
-      setTimeout(function() {
-        $modal.css("display", "none");
-      }, 400);
-    });
-  });
-});
 
