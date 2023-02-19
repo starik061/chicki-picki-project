3cfa2fa0036d3ec8a6a88bcb1b72db97cf0cbf95
starik061 <starik061@gmail.com>
2023-02-19 21:42:13 +0200

    test

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ $('.details-slider').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    prevArrow: "<img src='./img/details-of-our-restaurant/arrow-left.svg' class='prev' alt='left'>",
+    prevArrow: "<img src='./img/details-of-our-restaurant/cil_arrow-left.svg' class='prev' alt='left'>",
     nextArrow: "<img src='./img/details-of-our-restaurant/cil_arrow-right.svg' class='next' alt='right'>"
   });
 
