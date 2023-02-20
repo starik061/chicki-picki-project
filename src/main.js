a98c8b9fdda99b9c9c3ad939b279e182dada103b
KalchenkoNatalya <janatka2222@gmail.com>
2023-02-20 14:55:50 +0200

    стрілки з ховером

--- src/main.js
+++ src/main.js
@@ -1,20 +1,9 @@
 ///////////////Слайдер для секцій "детально про готель і ресторан"////////////////
 $('.details-slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    prevArrow: '.btn-to-left',
-    nextArrow: "<img src='img/details-of-our-restaurant/cil_arrow-right.svg' class='next' alt='right'>"
-  });
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: '<button type="button" class="btn-to-left"></button>',
+  nextArrow: '<button type="button" class="btn-to-right"></button>',
+});
 
- // prevArrow: '<button type="button" class="btn-to-left"></button>',
-
-  // $('.details-slider').slick({
-  //   infinite: true,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   prevArrow: "<img src='img/details-of-our-restaurant/cil_arrow-left.svg' class='prev' alt='left'>",
-  //   nextArrow: "<img src='img/details-of-our-restaurant/cil_arrow-right.svg' class='next' alt='right'>"
-  // });
-
- 
