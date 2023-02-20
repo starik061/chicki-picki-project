d1ab0ea3ae7ef04431ec1a1fd511b9f8e43fceac
starik061 <starik061@gmail.com>
2023-02-20 15:03:20 +0200

    Fix js for slider after merging

--- src/main.js
+++ src/main.js
@@ -4,11 +4,5 @@ import '/open-modal.js';
 import '/modal-table.js';
 
 ///////////////Слайдер для секцій "детально про готель і ресторан"////////////////
-$('.details-slider').slick({
-  infinite: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  prevArrow: '<button type="button" class="btn-to-left"></button>',
-  nextArrow: '<button type="button" class="btn-to-right"></button>',
-});
+
 
