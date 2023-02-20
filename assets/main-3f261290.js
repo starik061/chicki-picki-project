6d9b71d99b0df9d678ec2c7c4d87f76ed95cdb7c
starik061 <55664753+starik061@users.noreply.github.com>
2023-02-20 09:09:21 +0000

    Deploying to gh-pages from @ starik061/chicki-picki-project@8928a7bf4f16bccdbc4012a227b808140c33aa39 🚀

---
+++ assets/main-3f261290.js
@@ -0,0 +1,1 @@
+import"./main-a64e7dc4.js";import"./modal-table-f66c30b8.js";new Swiper(".swiper",{direction:"vertical",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
