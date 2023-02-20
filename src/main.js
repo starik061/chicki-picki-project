7825cea56c9038535b3e7fa32be0c184727edeb0
starik061 <starik061@gmail.com>
2023-02-20 11:43:07 +0200

    test with import js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
+import '/slider.js';
+import '/swiper-restaurant.js';
+import '/open-modal.js';
+import '/modal-table.js';
 
 ///////////////Слайдер для секцій "детально про готель і ресторан"////////////////
-$('.details-slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    prevArrow: "<img src='./img/details-of-our-restaurant/cil_arrow-left.svg' class='prev' alt='left'>",
-    nextArrow: "<img src='./img/details-of-our-restaurant/cil_arrow-right.svg' class='next' alt='right'>"
-  });
 
--- src/slider.js
+++ src/slider.js
@@ -9,10 +9,11 @@ $(document).ready(function () {
     // autoplaySpeed: 1500,
   });
 });
-/*$('.details-slider').slick({
+$('.details-slider').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     prevArrow: "<img src='./img/details-of-our-restaurant/cil_arrow-left.svg' class='prev' alt='left'>",
     nextArrow: "<img src='./img/details-of-our-restaurant/cil_arrow-right.svg' class='next' alt='right'>"
-  });*/
+  });
+
