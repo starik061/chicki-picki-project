9866ccd4d912febc2c6ff00c4c79502647b12453
starik061 <starik061@gmail.com>
2023-02-20 21:59:38 +0200

    test with mentor

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import '/slider.js';
-import '/swiper-restaurant.js';
-import '/open-modal.js';
-import '/modal-table.js';
+import './js/slider.js';
+// import '/../swiper-restaurant.js';
+import './js/open-modal.js';
+// import '/modal-table.js';
 
 ///////////////Слайдер для секцій "детально про готель і ресторан"////////////////
 
