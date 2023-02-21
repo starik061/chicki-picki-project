c7fac6315ab53c4181a17038d00c4d3e6186cf9d
starik061 <starik061@gmail.com>
2023-02-21 13:46:29 +0200

    add link to modal special js to main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './js/slider.js';
 // import '/../swiper-restaurant.js';
 import './js/open-modal.js';
-// import '/modal-table.js';
+import './js/modal-table.js';
 
 ///////////////Слайдер для секцій "детально про готель і ресторан"////////////////
 
